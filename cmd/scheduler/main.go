441f8b635a52123e62d07061f9089058dcaab304
Pavlek <bluseqicheng@gmail.com>
2026-08-28 19:54:27 +0800

    scheduler plugin

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/PavIek/gpusched/pkg/scheduler/plugins/gpufit"
 	"k8s.io/component-base/logs"
-	"sigs.k8s.io/scheduler-plugins/pkg/coscheduling"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 )
 
 func main() {
-	command := coscheduling.NewSchedulerCommand()
-
-	command.PluginRegistry = append(command.PluginRegistry, gpufit.NewPluginFactory())
+	command := app.NewSchedulerCommand(app.WithPlugin("GPUFit-plugin", gpufit.NewPluginFactory()))
 
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
 	if err := command.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 }
