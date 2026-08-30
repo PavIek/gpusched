c6a09334dbadbbc2318f896d91a9f8f3780c0ae1
Pavlek <bluseqicheng@gmail.com>
2026-08-30 23:15:56 +0800

    scheduler filter score plugin

--- pkg/scheduler/plugins/gpufit/gpufit.go
+++ pkg/scheduler/plugins/gpufit/gpufit.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/klog/v2"
 	fwk "k8s.io/kube-scheduler/framework"
@@ -18,8 +19,8 @@ type GPUFit struct {
 	frameworkHandler fwk.Handle
 }
 
-// var _ fwk.FilterPlugin = &GPUFit{}
-// var _ fwk.ScorePlugin = &GPUFit{}
+var _ fwk.FilterPlugin = &GPUFit{}
+var _ fwk.ScorePlugin = &GPUFit{}
 
 // var _ fwk.PreFilterPlugin = &GPUFit{}
 var _ fwk.EnqueueExtensions = &GPUFit{}
@@ -28,52 +29,50 @@ func (pl *GPUFit) Name() string {
 	return PluginName
 }
 
-// func (pl *GPUFit) Filter(ctx context.Context, state fwk.CycleState, pod *corev1.Pod, nodeInfo fwk.NodeInfo) *fwk.Status {
+func (pl *GPUFit) Filter(ctx context.Context, state fwk.CycleState, pod *v1.Pod, nodeInfo fwk.NodeInfo) *fwk.Status {
 
-// 	// requestedGPU := pod.Spec.Containers[0].Resources.Requests["nvidia.com/gpu"]
-// 	// if requestedGPU.IsZero() {
-// 	// 	return fwk.NewStatus(fwk.Success, "")
-// 	// }
+	requestedGPU := pod.Spec.Containers[0].Resources.Requests["nvidia.com/gpu"]
+	if requestedGPU.IsZero() {
+		return fwk.NewStatus(fwk.Success, "")
+	}
 
-// 	// allocatableGPUNum := nodeInfo.GetAllocatable().GetScalarResources()["nvidia.com/gpu"]
-// 	// requestedOnNodeNum := nodeInfo.GetRequested().GetScalarResources()["nvidia.com/gpu"]
+	allocatableGPUNum := nodeInfo.GetAllocatable().GetScalarResources()["nvidia.com/gpu"]
+	requestedOnNodeNum := nodeInfo.GetRequested().GetScalarResources()["nvidia.com/gpu"]
 
-// 	// if allocatableGPUNum < requestedOnNodeNum {
-// 	// 	return fwk.NewStatus(fwk.Unschedulable,
-// 	// 		fmt.Sprintf("insufficient GPU: available %d, requested %d", allocatableGPUNum, requestedOnNodeNum))
-// 	// }
+	if allocatableGPUNum < requestedOnNodeNum {
+		return fwk.NewStatus(fwk.Unschedulable,
+			fmt.Sprintf("insufficient GPU: available %d, requested %d", allocatableGPUNum, requestedOnNodeNum))
+	}
 
-// 	return fwk.NewStatus(fwk.Success, "")
-// }
+	return fwk.NewStatus(fwk.Success, "")
+}
 
-// func (pl *GPUFit) Score(ctx context.Context, state fwk.CycleState, pod *corev1.Pod, nodeInfo fwk.NodeInfo) (int64, *fwk.Status) {
+func (pl *GPUFit) Score(ctx context.Context, state fwk.CycleState, pod *v1.Pod, nodeInfo fwk.NodeInfo) (int64, *fwk.Status) {
+	nodeInfo, err := pl.frameworkHandler.SnapshotSharedLister().NodeInfos().Get(nodeInfo.Node().Name)
+	if err != nil {
+		return 0, fwk.NewStatus(fwk.Error, fmt.Sprintf("failed to get node info: %v", err))
+	}
 
-// 	// nodeInfo, err := pl.handle.SnapshotSharedLister().NodeInfos().Get(nodeInfo.Node().Name)
-// 	// if err != nil {
-// 	// 	return 0, fwk.NewStatus(fwk.Error, fmt.Sprintf("failed to get node info: %v", err))
-// 	// }
+	total := nodeInfo.GetAllocatable().GetScalarResources()["nvidia.com/gpu"]
+	if total == 0 {
+		return 0, fwk.NewStatus(fwk.Success, "")
+	}
+	total = 2
 
-// 	// total := nodeInfo.GetAllocatable().GetScalarResources()["nvidia.com/gpu"]
-// 	// if total == 0 {
-// 	// 	return 0, fwk.NewStatus(fwk.Success, "")
-// 	// }
-// 	// total = 2
+	usedNum := nodeInfo.GetRequested().GetScalarResources()["nvidia.com/gpu"]
 
-// 	// usedNum := nodeInfo.GetRequested().GetScalarResources()["nvidia.com/gpu"]
+	usedNum = 0
 
-// 	// usedNum = 0
+	utilization := float64(usedNum) / float64(total)
 
-// 	// utilization := float64(usedNum) / float64(total)
+	score := int64(utilization * 100)
 
-// 	//score := int64(utilization * 100)
-// 	var score int64 = 100
+	return score, fwk.NewStatus(fwk.Success, "")
+}
 
-// 	return score, fwk.NewStatus(fwk.Success, "")
-// }
-
-// func (pl *GPUFit) ScoreExtensions() fwk.ScoreExtensions {
-// 	return nil
-// }
+func (pl *GPUFit) ScoreExtensions() fwk.ScoreExtensions {
+	return nil
+}
 
 func New(ctx context.Context, obj runtime.Object, handle fwk.Handle) (fwk.Plugin, error) {
 	lh := klog.FromContext(ctx).WithValues("plugin", PluginName)
